4614d62d0ca4179ea970e68b6511909d6f35375f
Scott Brenner <scott@scottbrenner.me>
2017-07-30 03:40:08 -0700

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,6 +1,6 @@
 import os
 os.makedirs('~/.aws')
-f = open('~/.aws/credentials', 'w')
+f = open('~/.aws/credentials', 'r+')
 f.write('[default]')
 f.write('aws_access_key_id = ${AWS_ACCESS_KEY_ID}')
 f.write('aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}')
